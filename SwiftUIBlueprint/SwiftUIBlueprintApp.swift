362eaa80023d8e3ab7d661e084afc442d4be456e
D <D>
2022-04-19 11:03:58 +0200

    Part 1 - Project setup

---
+++ SwiftUIBlueprint/SwiftUIBlueprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBlueprintApp.swift
+//  SwiftUIBlueprint
+//
+//  Created by Dino Trnka on 19. 4. 2022..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBlueprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
